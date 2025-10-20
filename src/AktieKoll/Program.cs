58079720495300a92660fb3b99e6a041314b869a
Elie Bou Absi <135924664+Elieez@users.noreply.github.com>
2025-10-20 16:49:34 +0200

    Test

--- src/AktieKoll/Program.cs
+++ src/AktieKoll/Program.cs
@@ -34,7 +34,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
-
+// Rule TEST
 app.UseCors("AllowNextApp");
 
 app.UseHttpsRedirection();
