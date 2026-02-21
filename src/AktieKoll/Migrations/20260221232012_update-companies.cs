21c96eceb6c215cacc1496144933e58dd76de545
Elie Bou Absi <135924664+Elieez@users.noreply.github.com>
2026-02-22 00:28:42 +0100

    Feature/fix workflow (#172)
    * Fixed connectionstring
    
    * Added new migration, edited connection string

---
+++ src/AktieKoll/Migrations/20260221232012_update-companies.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AktieKoll.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatecompanies : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Company",
+                table: "Company");
+
+            migrationBuilder.RenameTable(
+                name: "Company",
+                newName: "Companies");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Company_Name",
+                table: "Companies",
+                newName: "IX_Companies_Name");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Company_Isin",
+                table: "Companies",
+                newName: "IX_Companies_Isin");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Company_Code",
+                table: "Companies",
+                newName: "IX_Companies_Code");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Companies",
+                table: "Companies",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Companies",
+                table: "Companies");
+
+            migrationBuilder.RenameTable(
+                name: "Companies",
+                newName: "Company");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Companies_Name",
+                table: "Company",
+                newName: "IX_Company_Name");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Companies_Isin",
+                table: "Company",
+                newName: "IX_Company_Isin");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Companies_Code",
+                table: "Company",
+                newName: "IX_Company_Code");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Company",
+                table: "Company",
+                column: "Id");
+        }
+    }
+}
