85336e103d40aea428b3e973a5112ac01d9fd102
Elie Bou Absi <135924664+Elieez@users.noreply.github.com>
2026-05-05 21:59:35 +0200

    Add TodayCount and TodayValue to YtdStats model

--- src/AktieKoll/Models/YtdStats.cs
+++ src/AktieKoll/Models/YtdStats.cs
@@ -1,8 +1,10 @@
-﻿namespace AktieKoll.Models;
+namespace AktieKoll.Models;
 
 public class YtdStats
 {
     public long TotalTransactions { get; set; }
     public decimal TotalValue { get; set; }
     public int UniqueCompanies { get; set; }
+    public int TodayCount { get; set; }
+    public decimal TodayValue { get; set; }
 }
